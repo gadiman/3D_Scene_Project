1122098c7dc45a27028d04b10d20c27a4a443d45
gad <gadiman@gmail.com>
2018-03-20 16:58:55 +0200

    update

---
+++ src/Primitives/Tests/CoordinateTest.java
@@ -0,0 +1,22 @@
+package Primitives.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateTest {
+
+    @org.junit.jupiter.api.Test
+    void getCoordinate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCoordinate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toString() {
+    }
+}
---
+++ src/Primitives/Tests/Point2DTest.java
@@ -0,0 +1,30 @@
+package Primitives.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Point2DTest {
+
+    @org.junit.jupiter.api.Test
+    void getX() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setX() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toString() {
+    }
+}
