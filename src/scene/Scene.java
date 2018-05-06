4975140c6f7d4fad57d6899570071b0a846ca2a0
gad <gadiman@gmail.com>
2018-05-06 17:45:32 +0300

    mo

---
+++ src/scene/Scene.java
@@ -0,0 +1,22 @@
+package scene;
+
+import Elements.Camera;
+import Geometries.Geometry;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene {
+
+    private String SceneName;
+    private Color BackgroundColor;
+    private List<Geometry> Geometrics= new ArrayList<Geometry>();
+    private Camera _camera;
+    private double ScreenDistance;
+
+
+    //**********************constructors***********************************//
+    public Scene
+
+}
